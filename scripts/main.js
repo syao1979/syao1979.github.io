f5cc147960e6ee3ef45230b463824b457102756f
unknown <syao1979@gmail.com>
2017-06-11 21:34:36 -0700

    mod tool update

--- scripts/main.js
+++ scripts/main.js
@@ -38,20 +38,25 @@
 					
 	//-- node customization
 	var callbacks = {};
-	function nodeBehave(node) {
+	function imgurl(type, gender) {
 		let imgURL = '/images/man.png'
-		if (node.data.type == "di-wang") {
-			if (node.data.gender == 'f') {
+		if (type == "di-wang") {
+			if (gender == 'f') {
 				imgURL = '/images/nv-wang.png'
 			} else {
 				imgURL = '/images/di-wang.png'
 			}
 		} else {
-			if (node.data.gender == 'f') {
+			if (gender == 'f') {
 				imgURL = '/images/woman.png'
 			}
 		}
-		
+		return imgURL
+	}
+	
+	function nodeBehave(node) {
+		//console.log(node.id)
+		let imgURL = imgurl(node.data.type, node.data.gender)		
 		if (node.data.image && node.data.image != "") {
 			imgURL = node.data.image
 		}
@@ -59,7 +64,12 @@
 		// This time it's a group of elements: http://www.w3.org/TR/SVG/struct.html#Groups
 		var ntxt = node.data.id
 		if (node.data.name) {
-			ntxt = node.data.name
+			let nlist = [node.data.id]
+			for(let n=0; n<node.data.name.length; n++){
+				nlist.push(node.data.name[n]);
+			}
+			
+			ntxt = nlist
 		}
 		var ui = Viva.Graph.svg('g'), // Create SVG text element with user name as content
 			svgText = Viva.Graph.svg('text').attr('y', '-4px').text(ntxt),
@@ -87,22 +97,30 @@
 	
 	//-- link display customization
 	function showLink(link){
-		if (link.data.relation == 'her-son') {	// do not draw link
+		if (link.data.hide) {	// do not draw link
 			return
 		}
 		
+		// dom element for link label; used in placeLink
+//		var label = Viva.Graph.svg('text').attr('id','label_'+link.data.id).text(link.data.id);
+//            	        graphics.getSvgRoot().childNodes[0].append(label);
+						
 		var ui = Viva.Graph.svg('path')
+		//console.dir(ui)
 		if(link.data.relation == 'wife') {
 			ui.attr('stroke', 'pink').attr('stroke-dasharray', '2, 2')
 		} else if (link.data.relation == 'son') {
 			ui.attr('stroke', 'red').attr('stroke-dasharray', '2, 2').attr('marker-end', 'url(#Triangle)');
 		} else if (link.data.type == "di2di") {
-			ui.attr('stroke', 'blue').attr('linewidth', 20)
+			ui.attr('stroke', 'blue')
 		} else {
 			ui.attr('stroke', 'gray').attr('stroke-dasharray', '5, 5');
 		}
 		ui.attr('fill', 'none')
-		ui.relation = link.data.relation	// remember for later use
+		//if (link.data.type == "di2di") {
+		//	ui.didi = true	// remember for later use
+		//}
+		
 		//return Viva.Graph.svg('path').attr('stroke', '')
 		//						return Viva.Graph.svg('path')
 		//                           .attr('stroke', 'red')
@@ -112,45 +130,48 @@
 	}
 	
 	function placeLink(linkUI, fromPos, toPos) {
-		var data = null
-		//if (linkUI.link.data.relation  == 'son' || linkUI.link.data.relation  == 'daughter') {
-		//	//arch edge
-		//	let ry = 15;
-		//	// using arc command: http://www.w3.org/TR/SVG/paths.html#PathDataEllipticalArcCommands
-		//	data = 'M' + fromPos.x + ',' + fromPos.y +
-		//			   ' A 10,' + ry + ',-30,0,1,' + toPos.x + ',' + toPos.y;
-		//} else {
-			let toNodeSize = nodeSize,
-				fromNodeSize = nodeSize;
-			
-			let from = geom.intersectRect(
-					// rectangle:
-							fromPos.x - fromNodeSize / 2, // left
-							fromPos.y - fromNodeSize / 2, // top
-							fromPos.x + fromNodeSize / 2, // right
-							fromPos.y + fromNodeSize / 2, // bottom
-					// segment:
-							fromPos.x, fromPos.y, toPos.x, toPos.y)
-					   || fromPos; // if no intersection found - return center of the node
-	
-			let to = geom.intersectRect(
-					// rectangle:
-							toPos.x - toNodeSize / 2, // left
-							toPos.y - toNodeSize / 2, // top
-							toPos.x + toNodeSize / 2, // right
-							toPos.y + toNodeSize / 2, // bottom
-					// segment:
-							toPos.x, toPos.y, fromPos.x, fromPos.y)
-						|| toPos; // if no intersection found - return center of the node
-	
-			
-			data = 'M' + from.x + ',' + from.y +
-					   'L' + to.x + ',' + to.y;
-		//}
+		var data = 'M' + fromPos.x + ',' + fromPos.y + 'L' + toPos.x + ',' + toPos.y;
 
-		if (data) {
-			linkUI.attr("d", data);
+		// make start and end points of link at outside center of the node image box
+		let toNodeSize = nodeSize,
+			fromNodeSize = nodeSize;
+		
+		let from = geom.intersectRect(
+				// rectangle:
+						fromPos.x - fromNodeSize / 2, // left
+						fromPos.y - fromNodeSize / 2, // top
+						fromPos.x + fromNodeSize / 2, // right
+						fromPos.y + fromNodeSize / 2, // bottom
+				// segment:
+						fromPos.x, fromPos.y, toPos.x, toPos.y)
+				   || fromPos; // if no intersection found - return center of the node
+
+		let to = geom.intersectRect(
+				// rectangle:
+						toPos.x - toNodeSize / 2, // left
+						toPos.y - toNodeSize / 2, // top
+						toPos.x + toNodeSize / 2, // right
+						toPos.y + toNodeSize / 2, // bottom
+				// segment:
+						toPos.x, toPos.y, fromPos.x, fromPos.y)
+					|| toPos; // if no intersection found - return center of the node
+
+
+		if (linkUI.link.data.type == "di2di") {	//di2di
+			data = 'M' + from.x + ',' + from.y + 'L' + to.x + ',' + (to.y + toNodeSize / 4);
+		} else if (linkUI.link.data.relation == "son" || linkUI.link.data.relation =="daughter" ){
+			data = 'M' + from.x + ',' + from.y + 'L' + to.x + ',' + (to.y - toNodeSize / 4);
+		} else {
+			data = 'M' + from.x + ',' + from.y + 'L' + to.x + ',' + to.y;
 		}
+		
+		linkUI.attr("d", data);
+		
+		
+		//add link label:
+//		 document.getElementById('label_'+linkUI.attr('id'))
+//                	.attr("x", (from.x + to.x) / 2)
+//                	.attr("y", (from.y + to.y) / 2);
 	}
 	
 	function desiredScale(layout, viewObj){
@@ -200,10 +221,13 @@
         //constructor: jHist,
 		init_display : function (gid){
 			this.graphics = Viva.Graph.View.svgGraphics();
-			
+			let self = this
 			geom = arrowMarker(this.graphics)
 			
-			this.graphics.node(nodeBehave).placeNode(placeNode).link(showLink).placeLink(placeLink);
+			this.graphics.node(nodeBehave)
+			.placeNode(placeNode)
+			.link(showLink)
+			.placeLink(placeLink);
 			
 			var layout_settings = {
 				springLength : this.VIEW_PARAM['springLength'],
@@ -215,7 +239,7 @@
 					//console.dir(link.data)
                     spring.length = this.VIEW_PARAM['springLength'] * link.data.timelength;
 					if (link.data.type == "di2di") {
-						spring.weight = 0
+						spring.weight = 0.1
 					}
                 }
 
@@ -245,23 +269,22 @@
 			},
 			
 		addSingleLink : function(link, draw){
-			if (link.relation != "di2di") {
+			if (link.type != "di2di") {
 				if (!link.timelength) {
 					link.timelength=10
 					if (link.relation == 'wife') {
 						link.timelength=4
 					}
 				}
-				if (link.type != 'noshow') {
-					this.graph.addLink(link.from, link.to, link)
-				}
-				this.graph.addLink(link.from, link.to, link);
-				if (draw) {
-					this.renderer.rerender()
-				}
-			} else {
-				link.timelength=4
 				this.graph.addLink(link.from, link.to, link)
+				
+			} else {
+				link.timelength = 4
+				this.graph.addLink(link.from, link.to, link)
+			}
+			
+			if (draw) {
+				this.renderer.rerender()
 			}
 		},
 		
@@ -272,17 +295,28 @@
 		},
 		
 		toJSON : function(){
+			return {"nodes" : this.nodeJSON(), "links": this.linkJSON()}
+		},
+		
+		nodeJSON : function(){
 			let nodes = []
-			let links = []
 			this.graph.forEachNode(function(node){
 				nodes.push(node.data)
 			})
-			this.graph.forEachLink(function(link){
-				links.push(link.data)
-			})
-			return {"nodes" : nodes, "links": links}
+			return nodes
 		},
-		listNodes : function (){
+		
+		linkJSON : function(){
+			let links = []
+			this.graph.forEachLink(function(link){
+				var ldata = jQuery.extend(true, {}, link.data)
+				delete ldata.timelength
+				links.push(ldata)
+			})
+			return links
+		},
+		
+		listNodeIDs : function (){
 			let nodes = []
 			this.graph.forEachNode((node) => {
 				nodes.push(node.id)
@@ -293,21 +327,38 @@
 		isrunning : function() { return this.animation; },
 		pause : function() { this.animation = false; this.renderer.pause(); },
 		resume : function() { this.animation = true; this.renderer.resume(); },
+		rerender : function() { this.renderer.rerender() },
 		
-		getNode : function(nid){ return },
-		setCallback : function(callbacktype, f){ callbacks[callbacktype] = f },
+		
+		getNode : function(nid){ return this.graph.getNode(nid) },
+		getNodePosition : function(nid) { return this.graphics.getNodeUI(nid).position; },
+		moveNodeTo : function(nid, x, y) { let ui=this.graphics.getNodeUI(nid); ui.position.x = x; ui.position.y = y; this.rerender(); return this; },
+		removeNode : function(nid) { let node = this.graph.getNode(nid); this.graph.removeNode(nid); return this;},
+		setCallback : function(callbacktype, f){ callbacks[callbacktype] = f; return this },
 		
         listLimits : function (){ return listLimits(this.graph, this.graphics) },
 		desiredScale : function () { return desiredScale(this.layout, this.viewObj) },
 		getGraphRectCenter : function() { return getGraphRectCenter(this.layout)},
 		
-		pinNode : function(node_id, pin) { this.layout.pinNode(this.graph.getNode(node_id), pin); return this },
+		pinNode : function(node_id, pin) { let n = this.graph.getNode(node_id); this.layout.pinNode(n, pin); n.pin = pin; },
+		
 		moveTo : function(node_id, x, y) { var nui = this.graphics.getNodeUI(node_id);
 											nui.position.x=x; nui.position.y=y;
-											this.renderer.rerender(); }
-		
+											this.renderer.rerender(); return this },
+		toggleRunState : function(){ if (this.animation){ this.pause() } else { this.resume() }; return this },
+		toggleNodePin : function(node_id) {let n = this.graph.getNode(node_id); this.layout.pinNode(n, !n.pin); n.pin = !n.pin; return this },
+		getLinks : function(from_id, to_id){
+			let node = this.graph.getNode(from_id)
+			let links = []
+			for (let n=0; n<node.links.length; n++){
+				if (node.links[n].toId == to_id) {
+					links.push(node.links[n])
+				}
+			}
+			return links
+		},
 	}
-	
+	 
     // Create a jMol object and put it i global scope
     return (window.jHist = new jHist());
 
